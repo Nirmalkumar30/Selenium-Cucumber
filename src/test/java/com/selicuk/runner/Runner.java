551e7d91993a8cb1b328c29e7b16a500ce393821
Nirmal-kumar-30 <nk75319@gmail.com>
2019-08-21 16:23:14 +0200

    Update Runner.java

--- src/test/java/com/selicuk/runner/Runner.java
+++ src/test/java/com/selicuk/runner/Runner.java
@@ -88,6 +88,7 @@ public class Runner extends BaseClass {
 	@Test(groups = "cucumber", description = "Runs Cucumber Feature", dataProvider = "features")
 	public void feature(CucumberFeatureWrapper cucumberFeature) {
 		driver = new ChromeDriver(options);
+		driver.get("https://accounts.google.com/ServiceLogin/identifier?service=mail&passive=true&rm=false&continue=https%3A%2F%2Fmail.google.com%2Fmail%2F&ss=1&scc=1&ltmpl=default&ltmplcache=2&emr=1&osid=1&flowName=GlifWebSignIn&flowEntry=AddSession");
 		testNGCucumberRunner.runCucumber(cucumberFeature.getCucumberFeature());
 		driver.quit();
 	}
